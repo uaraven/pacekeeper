1e90062e5106ada534e54b83b213db4e6b431a2e
Oleksiy Voronin <ovoronin@gmail.com>
2015-04-02 14:53:30 -0400

    Added screen inversion on select button long-click

--- src/pacekeeper.c
+++ src/pacekeeper.c
@@ -1,9 +1,12 @@
 #include "pebble.h"
 
 #define REPEAT_INTERVAL_MS 50
+#define LONG_CLICK_INTERVAL_MS 500
 
 // This is a custom defined key for saving our count field
 #define PACE_PKEY 1
+// Custom key for saving inversion status
+#define INVERT_PKEY 2
 
 // You can define defaults for values in persistent storage
 #define PACE_DEFAULT 90
@@ -24,8 +27,8 @@ static int s_step_interval = 0;
 static int s_bzz_count = 0;
 
 static bool s_running = false;
-
 static bool s_main_leg = true; // longer buzz for main leg, shorter for other leg
+static bool s_inverted = true;
 
 static void update_text() {
   static char s_text[6];
@@ -107,6 +110,11 @@ static void decrement_click_handler(ClickRecognizerRef recognizer, void *context
   update_text();
 }
 
+static void invert_click_handler(ClickRecognizerRef recognizer, void *context) {
+    s_inverted = !s_inverted;
+    layer_set_hidden((Layer *) s_inverter_layer, !s_inverted);      
+}
+
 static void start_stop_click_handler(ClickRecognizerRef recognizer, void *context) {
 	s_running = !s_running;
 	
@@ -121,17 +129,13 @@ static void click_config_provider(void *context) {
   window_single_repeating_click_subscribe(BUTTON_ID_DOWN, REPEAT_INTERVAL_MS, decrement_click_handler);
 	
   window_single_click_subscribe(BUTTON_ID_SELECT, start_stop_click_handler);
+  window_long_click_subscribe(BUTTON_ID_SELECT, LONG_CLICK_INTERVAL_MS, invert_click_handler, NULL);
 }
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-
-//   s_inverter_layer = inverter_layer_create(GRect(0, 0, 144, 168));
-//   layer_add_child(window_layer, inverter_layer_get_layer(s_inverter_layer));  
-//   layer_set_hidden((Layer *) s_inverter_layer, 0);    
     
   s_action_bar = action_bar_layer_create();
-  action_bar_layer_add_to_window(s_action_bar, window);
   action_bar_layer_set_click_config_provider(s_action_bar, click_config_provider);
 
   action_bar_layer_set_icon(s_action_bar, BUTTON_ID_UP, s_icon_plus);
@@ -142,20 +146,17 @@ static void main_window_load(Window *window) {
     
   s_header_layer = text_layer_create(GRect(4, 0, width, 30));
   text_layer_set_font(s_header_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-//  text_layer_set_background_color(s_header_layer, GColorClear);
   text_layer_set_text(s_header_layer, "Strides/minute");
   text_layer_set_text_alignment(s_header_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_header_layer));
 
   s_body_layer = text_layer_create(GRect(4, 44, width, 50));
   text_layer_set_font(s_body_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
-//  text_layer_set_background_color(s_body_layer, GColorClear);
   text_layer_set_text_alignment(s_body_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_body_layer));
     
   s_double_layer = text_layer_create(GRect(4, 95, width, 30));
   text_layer_set_font(s_double_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-//  text_layer_set_background_color(s_double_layer, GColorClear);
   text_layer_set_text_alignment(s_double_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_double_layer));
     
@@ -163,6 +164,13 @@ static void main_window_load(Window *window) {
   bitmap_layer_set_alignment(s_steps_layer, GAlignCenter);
   bitmap_layer_set_bitmap(s_steps_layer, NULL);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_steps_layer));
+    
+
+  s_inverter_layer = inverter_layer_create(GRect(0, 0, 144, 168));
+  layer_add_child(window_layer, inverter_layer_get_layer(s_inverter_layer));  
+  layer_set_hidden((Layer *) s_inverter_layer, !s_inverted);      
+
+  action_bar_layer_add_to_window(s_action_bar, window);
     
   update_text();
 }
@@ -176,7 +184,7 @@ static void main_window_unload(Window *window) {
 
   action_bar_layer_destroy(s_action_bar);
 
-//  inverter_layer_destroy(s_inverter_layer);
+  inverter_layer_destroy(s_inverter_layer);
 }
 
 static void init() {
@@ -191,6 +199,8 @@ static void init() {
   // Get the count from persistent storage for use if it exists, otherwise use the default
   s_pace = persist_exists(PACE_PKEY) ? persist_read_int(PACE_PKEY) : PACE_DEFAULT;
   calc_bzz();
+    
+  s_inverted = persist_exists(INVERT_PKEY) ? persist_read_bool(INVERT_PKEY) : true;
 
   s_main_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -203,6 +213,7 @@ static void init() {
 static void deinit() {
   // Save the count into persistent storage on app exit
   persist_write_int(PACE_PKEY, s_pace);
+  persist_write_bool(INVERT_PKEY, s_inverted);
 
   window_destroy(s_main_window);
 
